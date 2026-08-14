658ee3bbdacff78fed1c2472deedc9448d6ef310
morimotodev93 <bestlife.morimoto@gmail.com>
2026-08-14 12:19:47 +0900

    refactor: Text & semantic.css

--- src/components/primitives/Text/Text.types.ts
+++ src/components/primitives/Text/Text.types.ts
@@ -1,7 +1,15 @@
 import { ElementType, HTMLAttributes } from "react";
 
 export type TextSize = "xs" | "sm" | "base" | "lg" | "xl";
-export type TextColor = "default" | "muted" | "subtle";
+export type TextColor =
+  | "default"
+  | "muted"
+  | "subtle"
+  | "primary"
+  | "info"
+  | "success"
+  | "warning"
+  | "danger";
 export type TextWeight = "normal" | "medium" | "semibold";
 
 export type TextProps = HTMLAttributes<HTMLElement> & {
