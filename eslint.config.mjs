0a58149328277e4ba86425139a6f672a7962a147
morimotodev93 <bestlife.morimoto@gmail.com>
2026-08-22 09:17:08 +0900

    chore: eslint settings update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -9,6 +9,9 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    "dist/**",
+    "coverage/**",
+    "node_modules/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
