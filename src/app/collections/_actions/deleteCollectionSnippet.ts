1382c895aab21f63204c75b0c4558af787d77d48
morimotodev93 <bestlife.morimoto@gmail.com>
2026-09-05 09:54:18 +0900

    feat(collections): complete collection detail interactions

--- src/app/collections/_actions/deleteCollectionSnippet.ts
+++ src/app/collections/_actions/deleteCollectionSnippet.ts
@@ -7,10 +7,39 @@ export async function deleteCollectionSnippet(
   collectionId: string,
   collectionSnippetId: string,
 ) {
-  await prisma.collectionSnippet.delete({
-    where: {
-      id: collectionSnippetId,
-    },
+  await prisma.$transaction(async (tx) => {
+    const collectionSnippet = await tx.collectionSnippet.findUnique({
+      where: {
+        id: collectionSnippetId,
+      },
+      select: {
+        position: true,
+      },
+    });
+
+    if (!collectionSnippet) {
+      throw new Error("Collection snippet not found");
+    }
+
+    await tx.collectionSnippet.delete({
+      where: {
+        id: collectionSnippetId,
+      },
+    });
+
+    await tx.collectionSnippet.updateMany({
+      where: {
+        collectionId,
+        position: {
+          gt: collectionSnippet.position,
+        },
+      },
+      data: {
+        position: {
+          decrement: 1,
+        },
+      },
+    });
   });
 
   revalidatePath(`/collections/${collectionId}`);
